ad6c8d6cff2f118b6a67a6b3726f0091a7877e3a
Raheel Mughal <raheelmughal018@gmail.com>
2024-01-12 01:24:03 +0500

    all setup before controllers

---
+++ utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadFileOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload file on cloudinary
+    const res = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // files has been upoaded successfully
+    console.log("File is upload on Cloudinary", res.url);
+    return res;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    // remove the locally saved temporary fileas the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadFileOnCloudinary };
